635a1046ead63696c665a1b378254c990d99e9eb
stacherski <stacherski@gmail.com>
2022-11-02 19:47:14 +0100

    master

---
+++ script.js
@@ -0,0 +1,26 @@
+const APP = document.querySelector("#app");
+const FETCH_URL = "https://vanillajsacademy.com/api/dragons.json";
+
+const drawData = (data) => {
+  APP.innerHTML = `<h1>${data.publication}</h1>`;
+  APP.innerHTML += `${data.articles
+    .map((article) => {
+      let art = `<details open><summary>${article.title}</summary><h4>${article.author} @ ${article.pubdate}</h4><p>${article.article}</p><p><a href="${article.url}">Permalink</a></p></details>`;
+      return art;
+    })
+    .join("")}`;
+};
+const fetchData = async () => {
+  try {
+    let response = await fetch(FETCH_URL);
+    if (!response.ok) {
+      throw "It was all ok, and, ekhm... something went wrong, like, really wrong...";
+    }
+    let dragons = await response.json();
+    drawData(dragons);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+fetchData();
